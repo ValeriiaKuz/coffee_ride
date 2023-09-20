5e213acf97ca6acebfaba519e1d752bde4367f92
ValeriiaKuz <lerabukareva@icloud.com>
2023-09-20 12:51:02 +0400

    accept cookies modified

--- src/components/cookies-accept/cookies-accept.tsx
+++ src/components/cookies-accept/cookies-accept.tsx
@@ -40,8 +40,7 @@ export const CookiesAccept = () => {
         <p className={style.info}>
           This website uses cookies in order to offer you the most relevant
           information. Please accept cookies for optimal performance. By
-          continuing to use the website, you allow the use of cookie files. You
-          can always disable cookies in your browser settings.
+          continuing to use the website, you allow the use of cookie files.
         </p>
         <button onClick={acceptCookies} className={style.accept}>
           Accept cookies
