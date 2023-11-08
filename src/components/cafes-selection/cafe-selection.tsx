b203d9b0cc132610d173091de94a3a9d89e3ef40
ValeriiaKuz <lerabukareva@icloud.com>
2023-11-08 14:48:36 +0400

    refactoring firebase workflow

--- src/components/cafes-selection/cafe-selection.tsx
+++ src/components/cafes-selection/cafe-selection.tsx
@@ -1,10 +1,20 @@
-import { collection, getDocs } from "@firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "@firebase/firestore";
 import { db } from "@/src/firebase/firebase";
 import { CafeItem } from "@/src/components/cafes-selection/cafe-selection-item";
 import { CoffeePointType } from "@/src/servicies/redux/slices/coffeepoints";
 
 export const CafeSelection = async () => {
-  const coffeePointsData = await getDocs(collection(db, "selection"));
+  const coffeePointsIds = await getDoc(doc(db, "selection", "selected-cafes"));
+  const ids = coffeePointsIds.data() as { cafes: string[] };
+  const q = query(collection(db, "coffeepoints"), where("id", "in", ids.cafes));
+  const coffeePointsData = await getDocs(q);
   const coffeePoints = coffeePointsData.docs.map(
     (point) => point.data() as CoffeePointType,
   );
